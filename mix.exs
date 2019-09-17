6cac5a99f66d6621db419ceae59ea86e735bd649
Jacopo Cascioli <jacopo@jacopocascioli.com>
2019-09-17 17:41:26 +0200

    fix/mix: update minimum elixir version to 1.8

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Farseer.MixProject do
     [
       app: :farseer,
       version: "0.1.0",
-      elixir: "~> 1.7",
+      elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       description: description(),
       package: package(),
