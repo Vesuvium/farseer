c4000004c205fc1d7fe92519679634a22fe26750
Jacopo Cascioli <jacopo@jacopocascioli.com>
2019-09-19 14:04:37 +0200

    feat/handlers/s3: add S3 handler

---
+++ lib/farseer/handlers/s3.ex
@@ -0,0 +1,8 @@
+defmodule Farseer.Handlers.S3 do
+  @moduledoc """
+  The S3 handler can be used to interact with AWS S3-compliant services (S3 itself, Digitalocean spaces and so on). 
+  """
+  def handle(_conn, _options) do
+    :ok
+  end
+end
