170bb4824075a1fc5f83c3115f0ec947802d5962
Jacopo Cascioli <jacopo@jacopocascioli.com>
2019-12-31 21:16:33 +0100

    refactor/test/cli: use clearer names for tests

--- test/unit/cli_test.exs
+++ test/unit/cli_test.exs
@@ -4,40 +4,40 @@ defmodule FarseerTest.Cli do
 
   alias Farseer.Cli
 
-  test "the main method with no arguments" do
+  test "farseer" do
     dummy Cli, [{"help", fn -> :help end}] do
       assert Cli.main() == :help
     end
   end
 
   for t <- ["any", "help"] do
-    test "the main method with #{t} as argument" do
+    test "farseer #{t}" do
       dummy Cli, [{"help", fn -> :help end}] do
         assert Cli.main([unquote(t)]) == :help
       end
     end
   end
 
-  test "the main method with version argument" do
+  test "farseer version" do
     dummy Cli, [{"version", fn -> :version end}] do
       assert Cli.main(["version"]) == :version
     end
   end
 
-  test "the main method with --version argument" do
+  test "farseer --version" do
     dummy Cli, [{"version", fn -> :version end}] do
       assert Cli.main(["--version"]) == :version
     end
   end
 
-  test "the main method with run argument" do
+  test "farseer run" do
     dummy Farseer.Server, ["start/2"] do
       Cli.main(["run"])
       assert called(Farseer.Server.start(1, 2))
     end
   end
 
-  test "the version method" do
+  test "version/0" do
     dummy IO, ["puts"] do
       Cli.version()
       version = Application.spec(:farseer, :vsn)
@@ -45,7 +45,7 @@ defmodule FarseerTest.Cli do
     end
   end
 
-  test "the help method" do
+  test "help/0" do
     assert Cli.help() == :ok
   end
 end
