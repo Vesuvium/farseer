26deeec4c6795daf4f4fe17394b28b9109d8ae8f
Jacopo Cascioli <jacopo@jacopocascioli.com>
2020-01-04 16:26:14 +0100

    feat/ets: add Ets module

---
+++ lib/farseer/ets.ex
@@ -0,0 +1,38 @@
+defmodule Farseer.Ets do
+  @moduledoc """
+  Responsible for normalizing interactions with ets.
+  """
+  alias Farseer.Ets
+
+  def table, do: Confex.get_env(:farseer, :table)
+
+  def create_table do
+    :ets.new(Ets.table(), [:set, :protected, :named_table])
+  end
+
+  @doc """
+  The id of an entry is [method, path fragments]. For example,
+  GET "/hello/world" will be ["GET", "", "hello", "world"]
+  """
+  def id(method, path) do
+    [method | String.split(path, "/")] |> List.to_tuple()
+  end
+
+  def insert(method, path, path_rules, method_rules) do
+    :ets.insert(Ets.table(), {Ets.id(method, path), path_rules, method_rules})
+  end
+
+  @doc """
+  Gets all ets entries. Used for debug purposes.
+  """
+  def all() do
+    :ets.match(Ets.table(), {:"$1", :"$2", :"$3"})
+  end
+
+  @doc """
+  Matches a given method and path.
+  """
+  def match(method, path) do
+    :ets.lookup(Ets.table(), Ets.id(method, path))
+  end
+end
---
+++ test/unit/ets_test.exs
@@ -0,0 +1,45 @@
+defmodule FarseerTest.Ets do
+  use ExUnit.Case
+  import Dummy
+
+  alias Farseer.Ets
+
+  test "table/0" do
+    dummy Confex, [{"get_env", fn _a, _b -> :confex end}] do
+      assert Ets.table() == :confex
+      assert called(Confex.get_env(:farseer, :table))
+    end
+  end
+
+  test "id/2" do
+    assert Ets.id("GET", "/hello") == {"GET", "", "hello"}
+  end
+
+  test "insert/4" do
+    :ets.new(:test, [:set, :protected, :named_table])
+
+    dummy Ets, [{"table", fn -> :test end}, {"id", fn _a, _b -> :id end}] do
+      Ets.insert(:method, :path, :path_rules, :method_rules)
+      assert :ets.lookup(:test, :id) == [{:id, :path_rules, :method_rules}]
+    end
+  end
+
+  test "all/0" do
+    :ets.new(:test, [:set, :protected, :named_table])
+    :ets.insert(:test, {:id, 2, 3})
+
+    dummy Ets, [{"table", fn -> :test end}] do
+      assert Ets.all() == [[:id, 2, 3]]
+    end
+  end
+
+  test "match/2" do
+    id = {"GET", "", "/world"}
+    :ets.new(:test, [:set, :protected, :named_table])
+    :ets.insert(:test, {id, :path_rules, :method_rules})
+
+    dummy Ets, [{"table", fn -> :test end}, {"id", fn _a, _b -> id end}] do
+      assert Ets.match("GET", "/world") == [{id, :path_rules, :method_rules}]
+    end
+  end
+end
