58eeb2346a3dfcde694ef9d2b7829011204a69c8
Jacopo Cascioli <jacopo@jacopocascioli.com>
2020-01-23 11:25:41 +0100

    feat/mix/far.run: add far.run task

---
+++ lib/mix/far.run.ex
@@ -0,0 +1,17 @@
+defmodule Mix.Tasks.Far.Run do
+  use Mix.Task
+
+  @shortdoc "Runs farseer"
+
+  def run(args) do
+    Mix.Tasks.Run.run(run_args() ++ args)
+  end
+
+  defp run_args do
+    if iex_running?(), do: [], else: ["--no-halt"]
+  end
+
+  defp iex_running? do
+    Code.ensure_loaded?(IEx) and IEx.started?()
+  end
+end
