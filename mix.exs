738a55f92538dc37119433aa8c3270c478945b3d
Jacopo Cascioli <jacopo@jacopocascioli.com>
2020-01-23 01:08:01 +0100

    chore/mix: application runs Farseer.Application

--- mix.exs
+++ mix.exs
@@ -22,7 +22,8 @@ defmodule Farseer.MixProject do
 
   def application do
     [
-      extra_applications: [:logger]
+      extra_applications: [:logger],
+      mod: {Farseer.Application, []}
     ]
   end
 
