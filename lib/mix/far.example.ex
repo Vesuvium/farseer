aea542c3d4da7f7ce9b298b28e6565888774547d
Jacopo Cascioli <jacopo@jacopocascioli.com>
2020-01-23 11:29:59 +0100

    chore/mix: rename mix.far to mix.farseer

--- lib/mix/far.example.ex
+++
@@ -1,8 +0,0 @@
-defmodule Mix.Tasks.Far.Example do
-  @doc """
-  Creates an exampel farseer.yml file.
-  """
-  def run(_args) do
-    File.cp("priv/example.yml", "farseer-example.yml")
-  end
-end
--- lib/mix/far.help.ex
+++
@@ -1,8 +0,0 @@
-defmodule Mix.Tasks.Far.Help do
-  def run(_args) do
-    help =
-      "Usage: mix far[.COMMAND]\n\nCommands:\n\nrun\t\tRun farseer\nexample\t\tGenerate an example farseer.yml file\nversion\t\tPrint farseer's version"
-
-    IO.puts(help)
-  end
-end
