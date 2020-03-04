ecb6dd69c4f9c2ec16d12416203d3ae50001a2af
Jacopo Cascioli <jacopo@jacopocascioli.com>
2020-03-04 06:25:00 +0100

    feat/handlers/http: send with delete passes query_params to Tesla

--- test/unit/handlers/http_test.exs
+++ test/unit/handlers/http_test.exs
@@ -76,17 +76,23 @@ defmodule FarseerTest.Handlers.Http do
   end
 
   test "send/3 with DELETE" do
-    conn = %{:method => "DELETE"}
+    conn = %{:method => "DELETE", :query_params => "params"}
     path_rules = %{"to" => "path", "request_headers" => "request_headers"}
     method_rules = %{}
 
-    dummy Tesla, ["delete!/2"] do
-      dummy Headers, [{"process", fn _a, _b -> :headers end}] do
-        dummy Http, [{"to", fn _a, _b -> :to end}] do
-          Http.send(conn, path_rules, method_rules)
-          assert called(Headers.process(conn, path_rules))
-          assert called(Http.to(conn, "path"))
-          assert called(Tesla.delete!(:to, headers: :headers))
+    dummy Conn, [{"fetch_query_params", conn}] do
+      dummy Tesla, ["delete!/2"] do
+        dummy Headers, [{"process", fn _a, _b -> :headers end}] do
+          dummy Http, [{"to", fn _a, _b -> :to end}] do
+            Http.send(conn, path_rules, method_rules)
+            assert called(Conn.fetch_query_params(conn))
+            assert called(Headers.process(conn, path_rules))
+            assert called(Http.to(conn, "path"))
+
+            assert called(
+                     Tesla.delete!(:to, query: "params", headers: :headers)
+                   )
+          end
         end
       end
     end
