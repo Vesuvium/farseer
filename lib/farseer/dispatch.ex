84e83dc373465d7abda34caf625adb52e3d9dcf0
Jacopo Cascioli <jacopo@jacopocascioli.com>
2020-03-23 14:38:37 +0100

    feat/dispatch: call/2 uses the specified handler

--- lib/farseer/dispatch.ex
+++ lib/farseer/dispatch.ex
@@ -1,13 +1,22 @@
 defmodule Farseer.Dispatch do
   alias Farseer.Ets
-  alias Farseer.Handlers.Http
+  alias Farseer.Handlers.{Http, Json}
 
   def init(table), do: table
 
   def call(conn, _table) do
     case Ets.match(conn.method, conn.request_path) do
-      [{_path, path_rules, method_rules}] ->
-        Http.handle(conn, path_rules, method_rules)
+      [{_path, handler, path_rules, method_rules}] ->
+        cond do
+          handler == "Http" ->
+            Http.handle(conn, path_rules, method_rules)
+
+          handler == "Json" ->
+            Json.handle(conn, path_rules, method_rules)
+
+          true ->
+            Http.handle(conn, path_rules, method_rules)
+        end
 
       [] ->
         Http.not_found(conn)
